900085f78f801617f0040c5e1f82fa2e88977ad5
Moritz Patek <moritzpatek@MacBook-Air-von-Moritz.local>
2021-01-23 15:40:33 +0100

    Init

---
+++ main.py
@@ -0,0 +1,4 @@
+#so why am i doing that? Very simple. I have a few Instagram accounts and if you have those too
+#you will know that it is very time consuming sometimes (following and unfollowing people)
+#a while ago instagram had a good api that you could use but thats not the case anymore so lets
+#use webscraping instead.
