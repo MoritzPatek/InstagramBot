ca8b33fc62b563fd36f8b68fd3d7d7a692bd3656
Moritz Patek <moritzpatek@MacBook-Air-von-Moritz.local>
2021-01-23 18:03:26 +0100

    driver loop for multiple accounts

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 #you will know that it is very time consuming sometimes (following and unfollowing people)
 #a while ago instagram had a good api that you could use but thats not the case anymore so lets
 #use webscraping instead.
+
+from selenium import webdriver
+#you can eather way insert one account or multiple accounts the programm will adapt to the number of accounts
+usernames = [""]
+passwords = [""]
+
+drivers = []
+for i in usernames:
+    driver = webdriver.Chrome('/opt/homebrew/bin/chromedriver')
+    drivers.append(driver)
+
+for i in drivers:
+    i.get("https://www.instagram.com/")
+
+
